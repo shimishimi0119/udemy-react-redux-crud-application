657ef4b523a52d049d6bd3cafedb0d80b5d511e6
shimishimi0119 <ryota_shimizu@criticmission.jp>
2019-11-26 15:10:37 +0900

    study prop-types

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 function App() {
   const profiles = [
-    { name: 'taro', age: 12 },
-    { name: 'taro', age: 12 },
-    { name: 'taro', age: 12 },
-    { name: 'taro', age: 12 },
-    { name: 'taro', age: 12 },
     { name: 'taro', age: 12 },
     { name: 'jiro', age: 10 }
   ];
   return (
     <div>
       {profiles.map((e, index) => {
-        return <User name={e.name} age={e.age} />;
+        return <User name={e.name} age={e.age} key={index} />;
       })}
     </div>
   );
@@ -21,16 +18,19 @@ function App() {
 function User(props) {
   return (
     <div>
-      <p>
-        <div>ニャー,I am 「{props.name}」だニャー</div>
-        <div>{props.age} 才だニャー</div>
-        <div>テストは「{props.test}」点だニャー</div>
-      </p>
+      <p>ニャー,I am 「{props.name}」だニャー</p>
+      <p>{props.age} 才だニャー</p>
+      <p>テストは「{props.test}」点だニャー</p>
     </div>
   );
 }
+
 User.defaultProps = {
   test: 100
+};
+
+User.propTypes = {
+  name: PropTypes.string
 };
 
 // class App extends Component {
