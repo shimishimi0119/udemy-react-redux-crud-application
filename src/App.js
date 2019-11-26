18fb512c25606d4fdf2beee98f9736ba3dbed2d3
shimishimi0119 <ryota_shimizu@criticmission.jp>
2019-11-26 14:46:12 +0900

    component study

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react';
 
 function App() {
+  const profiles = [
+    { name: 'taro', age: 12 },
+    { name: 'taro', age: 12 },
+    { name: 'taro', age: 12 },
+    { name: 'taro', age: 12 },
+    { name: 'taro', age: 12 },
+    { name: 'taro', age: 12 },
+    { name: 'jiro', age: 10 }
+  ];
   return (
-    <div className="App">
-      <h1>hello world!!</h1>
+    <div>
+      {profiles.map((e, index) => {
+        return <User name={e.name} age={e.age} />;
+      })}
     </div>
   );
 }
+function User(props) {
+  return (
+    <div>
+      <p>
+        <div>ニャー,I am 「{props.name}」だニャー</div>
+        <div>{props.age} 才だニャー</div>
+        <div>テストは「{props.test}」点だニャー</div>
+      </p>
+    </div>
+  );
+}
+User.defaultProps = {
+  test: 100
+};
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text"></input>
+//         <button>button!!</button>
+//       </React.Fragment>
+//     );
+//   }
+// }
 
 export default App;
