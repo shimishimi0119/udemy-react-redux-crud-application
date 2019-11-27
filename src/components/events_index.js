d3a58be367387bc195a01ce9a35dc78945ebf3b4
shimishimi0119 <ryota_shimizu@criticmission.jp>
2019-11-27 12:58:55 +0900

    1258

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import { increment, decrement } from '../actions';
+import { readEvents } from '../actions';
 
 class EventsIndex extends Component {
+  componentDidMount() {
+    console.log('hi!');
+    this.props.readEvents();
+  }
+
   render() {
     const props = this.props;
 
     return (
       <React.Fragment>
-        <div>count : {props.value}</div>
+        <div>value : {props.value}</div>
         <button onClick={props.increment}>+1</button>
         <button onClick={props.decrement}>-1</button>
       </React.Fragment>
@@ -17,11 +22,13 @@ class EventsIndex extends Component {
   }
 }
 
-const mapStateToProps = state => ({ value: state.count.value });
-const mapDispatchToProps = dispatch => ({
-  increment: () => dispatch(increment()),
-  decrement: () => dispatch(decrement())
-});
+const mapStateToProps = state => ({});
+
+const mapDispatchToProps = ({ readEvents });
+
+// const mapDispatchToProps = dispatch => ({
+//   increment: () => dispatch(increment()),
+//   decrement: () => dispatch(decrement())
+// });
 
 export default connect(mapStateToProps, mapDispatchToProps)(EventsIndex);
-
