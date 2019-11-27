597dbe647e1c233e01218595aa2f5a873047319e
shimishimi0119 <ryota_shimizu@criticmission.jp>
2019-11-27 17:49:19 +0900

    add postEvent-Func

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -1,13 +1,50 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 
-// import { postEvent } from '../actions';
-// import { Link } from 'react-router-dom';
+import { postEvent } from '../actions';
 
 class EventsNew extends Component {
+  constructor(props) {
+    super(props);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+  renderField(field) {
+    const {
+      input,
+      label,
+      type,
+      meta: { touched, error }
+    } = field;
+    return (
+      <div>
+        <input {...input} placeholder={label} type={type} />
+        {touched && error && <span>{error}</span>}
+      </div>
+    );
+  }
+  async onSubmit(values) {
+    await this.props.postEvent(values);
+    this.props.history.push('/');
+  }
+
   render() {
+    const { handleSubmit } = this.props;
     return (
       <React.Fragment>
+        <form onSubmit={handleSubmit(this.onSubmit)}>
+          <div>
+            <Field label="Title" name="title" type="text" component={this.renderField} />
+          </div>
+          <div>
+            <Field label="Body" name="body" type="text" component={this.renderField} />
+          </div>
+          <div>
+            <input type="submit" value="Submit" disable={false} />
+            <Link to="/">キャンセル</Link>
+          </div>
+        </form>
         <div>foo</div>
       </React.Fragment>
     );
@@ -16,12 +53,14 @@ class EventsNew extends Component {
 
 // const mapStateToProps = state => ({ events: state.events });
 
-// const mapDispatchToProps = { postEvent };
+const mapDispatchToProps = { postEvent };
 
-// const mapDispatchToProps = dispatch => ({
-//   increment: () => dispatch(increment()),
-//   decrement: () => dispatch(decrement())
-// });
+const validate = values => {
+  const errors = {};
+  if (!values.title) errors.title = 'タイトルを入力してください';
+  if (!values.body) errors.body = 'ボディーを入力してください';
+  return errors;
+};
 
-export default connect(null, null)(EventsNew);
+export default connect(null, mapDispatchToProps)(reduxForm({ validate, form: 'eventNewForm' })(EventsNew));
 // export default connect(mapStateToProps, mapDispatchToProps)(EventsIndex);
